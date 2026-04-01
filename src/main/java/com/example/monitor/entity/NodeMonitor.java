ed5179fe6c83ff34c801cf8f9eec0b765ad49699
wqy <1404227367@qq.com>
2026-04-01 17:51:43 +0800

    没写接口的状态

--- src/main/java/com/example/monitor/entity/NodeMonitor.java
+++ src/main/java/com/example/monitor/entity/NodeMonitor.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import java.time.LocalDateTime;
 
 /**
  * 节点监控数据表（静态数据）
